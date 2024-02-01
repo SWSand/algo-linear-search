fd75309887f5ac0a70d39471cc5f2eba019c5f57
Jose Sandoval <Software.Sandoval@gmail.com>
2024-02-01 08:06:50 -0600

    solved

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,21 @@
 function linearSearch(searchTerm, arr) {
+
+  for(index in arr){
+    if(arr[index] === searchTerm){
+      return +(index)
+    }
+  }
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  ans = []
+  for(index in arr){
+    if(arr[index] === searchTerm){
+      ans.push(+(index))
+    }
+  }
+  return ans;
 }
 
 module.exports = { linearSearch, globalLinearSearch };
